ed78ff06a6f945edba7c9e29da301d5ec139777a
Rui <rui.filipe.machado@hotmail.com>
2016-12-08 22:50:53 +0000

    Algum refactoring

--- mvc/WeatherStation.java
+++ mvc/WeatherStation.java
@@ -15,27 +15,24 @@ import java.util.*;
 /**
  * Created by "amateur programmer”
  */
-public class WeatherStation implements WeatherObserver,Observable {
+public class WeatherStation implements WeatherObserver, Observable {
 
-
-
-    /**  Data objects
+    /**
+     * Data objects
      *
-     * Registos do valor de temperaturas
-     * Registos do valor de humidade
-     * Registos do valor de pressão atmosférica
-     * Registos de audio
-     * Registos de luminosidade
-     * */
-
-    HashMap<LocalDate,Vector<Integer>> temperatura;
-    HashMap<LocalDate,Vector<Integer>> humidade;
-    HashMap<LocalDate,Vector<Integer>> pressao_atm;
-    HashMap<LocalDate,Vector<Integer>> audio;
-    HashMap<LocalDate,Vector<Integer>> luminosidade;
+     * Registos do valor de temperaturas Registos do valor de humidade Registos
+     * do valor de pressão atmosférica Registos de audio Registos de
+     * luminosidade
+     *
+     */
+    HashMap<LocalDate, Vector<Integer>> temperatura;
+    HashMap<LocalDate, Vector<Integer>> humidade;
+    HashMap<LocalDate, Vector<Integer>> pressao_atm;
+    HashMap<LocalDate, Vector<Integer>> audio;
+    HashMap<LocalDate, Vector<Integer>> luminosidade;
     ArrayList<Observer> views;
     View view;
-    
+
     public WeatherStation() {
 
         this.humidade = new HashMap<>();
@@ -43,76 +40,101 @@ public class WeatherStation implements WeatherObserver,Observable {
         this.audio = new HashMap<>();
         this.luminosidade = new HashMap<>();
         this.pressao_atm = new HashMap<>();
-        this.views=new ArrayList<>();
-        this.view=new View(this);
+        this.views = new ArrayList<>();
+        this.view = new View(this);
+    }
+    
+    /**
+     * Actualiza valores de temperatura
+     */
+    void actualizaTemperatura(Vector<Integer> values) {
+        if (this.temperatura.get(LocalDate.now()) != null) {
+            Vector<Integer> val_temp = temperatura.get(LocalDate.now());
+            val_temp.add(values.elementAt(0));
+        } else {
+            Vector<Integer> new_vector = new Vector<Integer>();
+            new_vector.add(values.elementAt(0));
+            temperatura.put(LocalDate.now(), new_vector);
+        }
+    }
+    
+    /**
+     * Actualiza valores de humidade
+     */
+    void actualizaHumidade(Vector<Integer> values) {
+        if (this.humidade.get(LocalDate.now()) != null) {
+            Vector<Integer> val_temp = humidade.get(LocalDate.now());
+            val_temp.add(values.elementAt(1));
+        } else {
+            Vector<Integer> new_vector = new Vector<Integer>();
+            new_vector.add(values.elementAt(1));
+            humidade.put(LocalDate.now(), new_vector);
+        }
+    }
+    /**
+     * Actualiza valores de pressao
+     */
+    void actualizaPressao_atm(Vector<Integer> values) {
+        if (this.pressao_atm.get(LocalDate.now()) != null) {
+            Vector<Integer> val_temp = pressao_atm.get(LocalDate.now());
+            val_temp.add(values.elementAt(2));
+        } else {
+            Vector<Integer> new_vector = new Vector<Integer>();
+            new_vector.add(values.elementAt(2));
+            pressao_atm.put(LocalDate.now(), new_vector);
+        }
+    }
+    
+    /**
+     * Actualiza valores de audio
+     */
+    void actualizaAudio(Vector<Integer> values) {
+
+        if (this.audio.get(LocalDate.now()) != null) {
+            Vector<Integer> val_temp = audio.get(LocalDate.now());
+            val_temp.add(values.elementAt(3));
+        } else {
+            Vector<Integer> new_vector = new Vector<Integer>();
+            new_vector.add(values.elementAt(3));
+            audio.put(LocalDate.now(), new_vector);
+        }
+    }
+    /**
+     * Actualiza valores de luminosidade
+     */
+    void actualizaLuminosidade(Vector<Integer> values) {
+
+        if (this.luminosidade.get(LocalDate.now()) != null) {
+            Vector<Integer> val_temp = luminosidade.get(LocalDate.now());
+            val_temp.add(values.elementAt(4));
+        } else {
+            Vector<Integer> new_vector = new Vector<Integer>();
+            new_vector.add(values.elementAt(4));
+            luminosidade.put(LocalDate.now(), new_vector);
+        }
     }
 
     /**
-     * A estção meteriologica recebe updates dos sensores e armazena os valores na estrutura de dados respectiva.
+     * A estção meteriologica recebe updates dos sensores e armazena os valores
+     * na estrutura de dados respectiva.
+     *
      * @param id identificador do sensor XDK
-     * @param values parametros lidos pelo sensor
-     *               posição 0 : temperatura
-     *               posição 1 : humidade
-     *               posição 2 : pressão atmosféria
-     *               posição 3 : audio
-     *               posição 4 : luminosidade
+     * @param values parametros lidos pelo sensor posição 0 : temperatura
+     * posição 1 : humidade posição 2 : pressão atmosféria posição 3 : audio
+     * posição 4 : luminosidade
      *
      */
     @Override
-    public void update(int id, Vector<Integer> values){
-       
-       
-       switch (id) {
+    public void update(int id, Vector<Integer> values) {
+
+        switch (id) {
             case 1: // XDK exterior
-                
-                
-                if (this.temperatura.get(LocalDate.now()) != null){
-                    Vector<Integer> val_temp = temperatura.get(LocalDate.now());
-                    val_temp.add(values.elementAt(0));
-                } else
-                {
-                    Vector<Integer> new_vector  =  new Vector<Integer>();
-                    new_vector.add(values.elementAt(0));
-                    temperatura.put(LocalDate.now(), new_vector);
-                }
-                if (this.humidade.get(LocalDate.now()) != null){
-                    Vector<Integer> val_temp = humidade.get(LocalDate.now());
-                    val_temp.add(values.elementAt(1));
-                } else
-                {
-                    Vector<Integer> new_vector  =  new Vector<Integer>();
-                    new_vector.add(values.elementAt(1));
-                    humidade.put(LocalDate.now(), new_vector);
-                }
 
-                if (this.pressao_atm.get(LocalDate.now()) != null){
-                    Vector<Integer> val_temp = pressao_atm.get(LocalDate.now());
-                    val_temp.add(values.elementAt(2));
-                } else
-                {
-                    Vector<Integer> new_vector  =  new Vector<Integer>();
-                    new_vector.add(values.elementAt(2));
-                    pressao_atm.put(LocalDate.now(), new_vector);
-                }
-
-                if (this.audio.get(LocalDate.now()) != null){
-                    Vector<Integer> val_temp = audio.get(LocalDate.now());                
-                    val_temp.add(values.elementAt(3));
-                } else
-                {
-                    Vector<Integer> new_vector  =  new Vector<Integer>();
-                    new_vector.add(values.elementAt(3));
-                    audio.put(LocalDate.now(), new_vector);
-                }
-                if (this.luminosidade.get(LocalDate.now()) != null){
-                    Vector<Integer> val_temp = luminosidade.get(LocalDate.now());
-                    val_temp.add(values.elementAt(4));
-                } else
-                {
-                    Vector<Integer> new_vector  =  new Vector<Integer>();
-                    new_vector.add(values.elementAt(4));
-                    luminosidade.put(LocalDate.now(), new_vector);
-                }
+                actualizaTemperatura(values);
+                actualizaHumidade(values);
+                actualizaPressao_atm(values);
+                actualizaAudio(values);
+                actualizaLuminosidade(values);
                 notifyObservers();
                 break;
             case 2: // XDK interior
@@ -124,19 +146,18 @@ public class WeatherStation implements WeatherObserver,Observable {
         }
     }
 
-    
     @Override
     public void notifyObservers() {
         int i;
 
         for (i = 0; i < views.size(); i++) {
             Observer ob = (Observer) views.get(i);
-            ob.update(temperatura,humidade,pressao_atm,audio,luminosidade);
-           
+            ob.update(temperatura, humidade, pressao_atm, audio, luminosidade);
+
         }
     }
 
-
+    @Override
     public void removeObserver(Observer observer) {
         this.views.remove(observer);
     }
@@ -145,13 +166,5 @@ public class WeatherStation implements WeatherObserver,Observable {
     public void registerObserver(Observer observer) {
         this.views.add(observer);
     }
-
-
-    
-
- 
-
-
- 
 
 }
